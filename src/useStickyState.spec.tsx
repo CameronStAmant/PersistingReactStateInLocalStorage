71df7a8727f68cc34a45c47929d4b936cdc6cc4b
Cameron St Amant <40364114+CameronStAmant@users.noreply.github.com>
2023-03-02 07:20:36 -0700

    Update spec

--- src/useStickyState.spec.tsx
+++ src/useStickyState.spec.tsx
@@ -2,12 +2,11 @@ import { renderHook } from "@testing-library/react";
 import { UseStickyState } from "./UseStickyState";
 
 describe("sticky state", () => {
-  test("value is sent to localStorage", () => {
+  it("is sent to and retrieved from localStorage", () => {
     const setItem = jest.spyOn(window.localStorage.__proto__, "setItem");
     const getItem = jest.spyOn(window.localStorage.__proto__, "getItem");
-    const { result } = renderHook(() => UseStickyState(50, "count"));
 
-    console.log(result.current);
+    const { result } = renderHook(() => UseStickyState(50, "count"));
 
     expect(setItem).toHaveBeenCalled();
     expect(getItem).toHaveBeenCalled();
